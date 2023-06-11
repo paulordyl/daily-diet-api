7c38f4dd9fd6838a662d60dc68998bc3b04699eb
Paulo Lacerda <paulorl2810@gmail.com>
2023-06-10 22:10:04 -0300

    Finish tests

--- test/meals.spec.ts
+++ test/meals.spec.ts
@@ -45,4 +45,139 @@ describe('Meals routes', () => {
       }),
     )
   })
+
+  it('should be able to update a meal', async () => {
+    const createUserResponse = await request(app.server)
+      .post('/users')
+      .send({
+        name: 'Jhon Doe',
+      })
+      .expect(201)
+
+    const cookies = createUserResponse.get('Set-Cookie')
+
+    const createMealResponse = await request(app.server)
+      .post('/meals')
+      .set('Cookie', cookies)
+      .send({
+        name: 'Pao',
+        description: 'Pao com manteiga',
+        inDiet: false,
+      })
+      .expect(200)
+
+    const { meal } = createMealResponse.body
+
+    const updateMealResponse = await request(app.server)
+      .put(`/meals/${meal.id}`)
+      .set('Cookie', cookies)
+      .send({
+        name: 'Pao com manteiga',
+      })
+      .expect(200)
+
+    expect(updateMealResponse.body.meal).toEqual(
+      expect.objectContaining({
+        name: 'Pao com manteiga',
+      }),
+    )
+  })
+
+  it('should be able to delete a meal', async () => {
+    const createUserResponse = await request(app.server)
+      .post('/users')
+      .send({
+        name: 'Jhon Doe',
+      })
+      .expect(201)
+
+    const cookies = createUserResponse.get('Set-Cookie')
+
+    const createMealResponse = await request(app.server)
+      .post('/meals')
+      .set('Cookie', cookies)
+      .send({
+        name: 'Pao',
+        description: 'Pao com manteiga',
+        inDiet: false,
+      })
+      .expect(200)
+
+    const { meal } = createMealResponse.body
+
+    await request(app.server)
+      .delete(`/meals/${meal.id}`)
+      .set('Cookie', cookies)
+      .expect(201)
+  })
+
+  it('should be able to list all meals', async () => {
+    const createUserResponse = await request(app.server)
+      .post('/users')
+      .send({
+        name: 'Jhon Doe',
+      })
+      .expect(201)
+
+    const cookies = createUserResponse.get('Set-Cookie')
+
+    const createMealResponse = await request(app.server)
+      .post('/meals')
+      .set('Cookie', cookies)
+      .send({
+        name: 'Pao',
+        description: 'Pao com manteiga',
+        inDiet: false,
+      })
+      .expect(200)
+
+    const { meal } = createMealResponse.body
+
+    const listMealsResponse = await request(app.server)
+      .get('/meals')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(listMealsResponse.body.meals).toEqual([
+      expect.objectContaining({
+        name: meal.name,
+        description: meal.description,
+      }),
+    ])
+  })
+
+  it('shoud be able to list a specific meal', async () => {
+    const createUserResponse = await request(app.server)
+      .post('/users')
+      .send({
+        name: 'Jhon Doe',
+      })
+      .expect(201)
+
+    const cookies = createUserResponse.get('Set-Cookie')
+
+    const createMealResponse = await request(app.server)
+      .post('/meals')
+      .set('Cookie', cookies)
+      .send({
+        name: 'Pao',
+        description: 'Pao com manteiga',
+        inDiet: false,
+      })
+      .expect(200)
+
+    const { meal } = createMealResponse.body
+
+    const listMealResponse = await request(app.server)
+      .get(`/meals/${meal.id}`)
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(listMealResponse.body.meal).toEqual(
+      expect.objectContaining({
+        name: meal.name,
+        description: meal.description,
+      }),
+    )
+  })
 })
--- test/users.spec.ts
+++ test/users.spec.ts
@@ -1,4 +1,4 @@
-import { afterAll, beforeAll, beforeEach, describe, it } from 'vitest'
+import { afterAll, beforeAll, beforeEach, describe, expect, it } from 'vitest'
 import { execSync } from 'node:child_process'
 import request from 'supertest'
 import { app } from '../src/app'
@@ -25,5 +25,70 @@ describe('Users routes', () => {
         name: 'Jhon Doe',
       })
       .expect(201)
+  })
+
+  it('should be able to get user metrics', async () => {
+    const createUserResponse = await request(app.server)
+      .post('/users')
+      .send({
+        name: 'Jhon Doe',
+      })
+      .expect(201)
+
+    const cookies = createUserResponse.get('Set-Cookie')
+
+    await request(app.server)
+      .post('/meals')
+      .set('Cookie', cookies)
+      .send({
+        name: 'Pao',
+        description: 'Pao com manteiga',
+        inDiet: false,
+      })
+      .expect(200)
+
+    await request(app.server)
+      .post('/meals')
+      .set('Cookie', cookies)
+      .send({
+        name: 'Peira',
+        description: 'Peira',
+        inDiet: true,
+      })
+      .expect(200)
+
+    await request(app.server)
+      .post('/meals')
+      .set('Cookie', cookies)
+      .send({
+        name: 'Banana',
+        description: 'Banana',
+        inDiet: true,
+      })
+      .expect(200)
+
+    await request(app.server)
+      .post('/meals')
+      .set('Cookie', cookies)
+      .send({
+        name: 'Sorvete',
+        description: 'Sorvete',
+        inDiet: false,
+      })
+      .expect(200)
+
+    const userMetricsResponse = await request(app.server)
+      .get('/users/metrics')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(userMetricsResponse.body).toEqual(
+      expect.objectContaining({
+        totalMeals: 4,
+        totalMealsInDiet: 2,
+        totalMealsNotInDiet: 2,
+        bestSequence: 2,
+      }),
+    )
   })
 })
